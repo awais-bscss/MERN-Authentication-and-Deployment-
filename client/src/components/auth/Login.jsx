3087fcbca39f5a660010c41810c431963bb8367c
Muhammad Awais <kingyt6624@gmail.com>
2025-08-18 21:34:03 +0500

    feat: implemented user login and registration forms

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFromData] = useState({
+    name: "",
+    email: "",
+  });
+  const { email, password } = formData;
+  const onChange = (e) => {
+    setFromData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("user login ");
+  };
+  return (
+    <div>
+      <h1>Log In</h1>
+      <p>Log in to your Acount</p>
+      <form onSubmit={(e) => onSubmit(e)} action="POST">
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Adress"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder=" Password"
+            value={password}
+            onChange={(e) => onChange(e)}
+            required
+            minLength="6"
+          />
+        </div>
+        <input type="submit" value="Log in" />
+        <p>
+          Not Registered Yet? <Link to="/regiter">Register</Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,7 +1,84 @@
 import React from "react";
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
 const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFromData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmpass: "",
+  });
+  const { name, email, password, confirmpass } = formData;
+
+  const onChange = (e) => {
+    setFromData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== confirmpass) {
+      alert("passwors must match");
+    } else {
+      console.log(formData);
+    }
+  };
+  return (
+    <div>
+      <h1>Register Your Acount</h1>
+
+      <form onSubmit={(e) => onSubmit(e)} action="POST">
+        <div>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your Name"
+            value={name}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Adress"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            placeholder=" Password"
+            value={password}
+            onChange={(e) => onChange(e)}
+            required
+            minLength="6"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="confirmpass"
+            placeholder=" Confirm Password"
+            value={confirmpass}
+            onChange={(e) => onChange(e)}
+            required
+            minLength="6"
+          />
+        </div>
+        <input type="submit" value="Register" />
+        <p>
+          Already Have an Acount? <Link to="/login"> Log In</Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
